91f3619a41b27afb1f6c52993e448313834f6584
wh1t3h47 <tom.mharres@gmail.com>
2023-10-26 22:01:33 -0300

    fix(shimmers): Walle e Eve :wrench:

--- src/components/templates/ShimmerWalleTemplate.tsx
+++ src/components/templates/ShimmerWalleTemplate.tsx
@@ -6,7 +6,7 @@ export const ShimmerWalleeTemplate: React.FC = () => (
   <Suspense
     fallback={
       <div className="flex justify-center h-full">
-        <div className="animate-pulse bg-gray-400 w-walleSize h-walleSize h-350 rounded-lg shadow-lg mb-8"></div>
+        <div className="animate-pulse bg-gray-400 w-walleSize h-walleSize rounded-lg shadow-lg mb-8"></div>
       </div>
     }
   >
